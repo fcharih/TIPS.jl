76709bedec8faf6c344820dadb50ee268ab7f715
Francois Charih <francois.charih@gmail.com>
2022-02-04 23:24:39 -0500

    Update.

--- run_mpi.sh
+++ run_mpi.sh
@@ -1,8 +1,8 @@
-/home/fcharih/.julia/bin/mpiexecjl -n 4 julia --project -t 20 scripts/evolve_mpi.jl \
+/home/fcharih/.julia/bin/mpiexecjl -n 2 julia --project -t 6 scripts/evolve_mpi.jl \
   -p 2000 \
   -i Q9H7B4 \
   -o data/smyd3-3 \
-  -f ../SPRINT.jl/data/sequences/proteome.fasta \
-  -s ../SPRINT.jl/data/hsps/proteome_sprintjl_processed.hsp \
-  -t ../SPRINT.jl/data/interactions.txt \
+  -f data/proteome.fasta \
+  -s data/proteome_sprintjl_processed.hsp \
+  -t data/interactions.txt \
   --convergence_iterations 50
--- scripts/evolve_mpi.jl
+++ scripts/evolve_mpi.jl
@@ -13,18 +13,19 @@ using SPRINT.DataTypes
 EXTRACTION_TAG = 1001
 PREDICTION_TAG = 1002
 
-struct SequencesAndHsps
+struct Payload
     sequences::Vector{Protein}
     hsps::Set{HSP}
 end
 
-function dispatch_hsp_extraction(peptides, comm)
-    for slave_id in 1:MPI.Comm_size(comm)-1
-        MPI.send(peptides, slave_id, MPI.MPI_ANY_TAG, comm)
+function dispatch_hsp_extraction(sequences, comm)
+    for worker_id in 1:MPI.Comm_size(comm)-1
+        MPI.send(sequences, worker_id, EXTRACTION_TAG, comm)
+        @info "Dispatched sequences to worker $(worker_id)..."
     end
 
     hsps = Extraction.extract_hsps(
-        proteins;
+        sequences;
         smer_sim_threshold = args["smer_similarity_threshold"],
         hsp_sim_threshold = args["hsp_similarity_threshold"],
         kmer_size = args["kmer_size"],
@@ -36,63 +37,64 @@ function dispatch_hsp_extraction(peptides, comm)
         as_matrix = false
     )
 
-    for slave_id in 1:MPI.Comm_size(comm)-1
-        peptide_hsps, status = MPI.recv(slave_id, MPI.MPI_ANY_TAG, comm)
+    @info "Main process done extracting hsps."
+
+    for worker_id in 1:MPI.Comm_size(comm)-1
+        peptide_hsps, status = MPI.recv(worker_id, MPI.MPI_ANY_TAG, comm)
+        @info "Main processed received HSPs from worker $(worker_id)..."
         union!(hsps, peptide_hsps)
     end
 
     hsps
 end
 
-function dispatch_prediction(proteins, peptides, hsps, training_pairs, comm, args)
+function dispatch_prediction(sequences, training_hsps, peptide_hsps, training_pairs, comm, args)
     
-    payload = SequencesAndHsps([proteins..., peptides...], hsps)
+    payload = Payload(sequences, peptide_hsps)
 
-    for slave_id in 1:MPI.Comm_size(comm)-1
-        MPI.send(paylod, slave_id, PREDICTION_TAG, comm)
+    for worker_id in 1:MPI.Comm_size(comm)-1
+        MPI.send(payload, worker_id, PREDICTION_TAG, comm)
     end
 
     matrix = Prediction.score_interactions(
         payload.sequences,
-        hsps,
+        union(peptide_hsps, training_hsps),
         training_pairs[1:MPI.Comm_size(comm):end],
         args["kmer_size"]
     )
 
-    for slave_id in 1:MPI.Comm_size(comm)-1
-        sub_matrix, status = MPI.recv(slave_id, MPI.MPI_ANY_TAG, comm)
+    for worker_id in 1:MPI.Comm_size(comm)-1
+        sub_matrix, status = MPI.recv(worker_id, MPI.MPI_ANY_TAG, comm)
         matrix += sub_matrix
     end
 
-    matrix
+    protein_ids = Dict(p.protein_id => i for (i, p) in enumerate(sequences))
+
+    ScoreMatrix(protein_ids, matrix)
 end
 
+function isconverged(summary_file, args)::Bool
+    evolution = CSV.File(summary_file) |> DataFrame
 
-function master_process(comm, args)
-
-    rank = MPI.Comm_rank(comm)
-
-    @info "Loading the proteome sequences..."
-    proteins = FileIO.load_sequences(args["fasta"])
-
-    @info "Loading the training pairs..."
-    training_pairs = FileIO.load_training_pairs(args["training_pairs"])
-
-    @info "Loading the HSPs extracted from the proteome..."
-    training_hsps = FileIO.load_hsps(args["hsps"], proteins)
-    @info "Loaded $(length(hsps)) HSPs..."
-
-    @info "Creating a directory structure for the evolution results..."
-    if isdir(args["output"])
-        @error "$(args["output"]) already exists and cannot be overwritten."
-        return
+    if nrow(evolution) < args["min_generations"]
+        return false
     end
 
-    mkdir(args["output"])
+    # Compute fitness delta
+    Δfitness = []
+    for i in 2:nrow(evolution)
+        push!(Δfitness, evolution.fitness[i] - evolution.fitness[i-1])
+    end
 
-    run_dir = "$(args["output"])/run$(run_index)"
+    if all(x -> x < args["convergence_variation"], Δfitness[end - args["convergence_iterations"]:end])
+        return true
+    end
+end
+
+function run(run_id, comm, args)
+
+    run_dir = "$(output_dir)/run$(run_id)"
     summary_file = "$(run_dir)/summary.csv"
-    mkdir(run_dir)
     mkdir("$(run_dir)/peptides")
 
     if args["save_matrices"]
@@ -105,12 +107,113 @@ function master_process(comm, args)
     Random.seed!(seed)
     peptides = [random_peptide("0-$(i)", args["peptide_length"]) for i in 1:args["population_size"]]
 
-    hsps = dispatch_hsp_extraction(peptides, comm)
-    score_matrix = dispatch_prediction(proteins, peptides, hsps, training_pairs, comm, args)
+    sequences = [deepcopy(proteins); [create_protein(p.id, p.sequence; new_sequence=true) for p in peptides]]
+    peptide_hsps = dispatch_hsp_extraction(sequences, comm)
+    score_matrix = dispatch_prediction(sequences, training_hsps, peptide_hsps, training_pairs, comm, args)
 
+    fitness_expression = Meta.parse(args["fitness_function"])
+    fitness_function = @eval function(ts, tr, mots, motr, n_prot)
+        $fitness_expression
+    end
+
+    @info "Computing the fitness of ancestral peptides using computed interaction scores..."
+    compute_fitness!(peptides, args["target_id"], score_matrix, fitness_function)
+
+    # Initialize tournament
+    tournament_function = tournament(args["tournament_size"], false)
+
+    generation = 1
+    fittest_to_date = -Inf32
+    iterations_since_change = 0
+    while true
+        @info "ITERATION $(generation) STARTED..."
+        start_time = time()
+
+        @info "Applying genetic operators to generate the next generation..."
+        offspring::Vector{Peptide} = []
+
+        # Peptide selection
+        fitnesses = [peptide.fitness for peptide in peptides]
+        peptide_selection = tournament_function(fitnesses, args["population_size"])
+
+        for (i, index_of_selected) in enumerate(peptide_selection)
+            peptide_id = "$(generation)-$(length(offspring) + 1)"
+            push!(offspring, Peptide(peptide_id, peptides[index_of_selected].sequence))
+        end
+
+        # Crossover
+        shuffle!(offspring)
+        for i in 1:2:floor(Int(length(offspring)/2))
+            crossover!(offspring[i], offspring[i + 1])
+        end
+
+        # Mutate
+        for peptide in offspring
+            if rand(Float64) < args["mutation_probability"]
+                mutate!(peptide, args["mutation_rate"])
+            end
+        end
+
+        @info "Computing the interaction scores of newly generated peptides..."
+        sequences = [deepcopy(proteins); [create_protein(p.id, p.sequence; new_sequence=true) for p in offspring]]
+        peptide_hsps = dispatch_hsp_extraction(sequences, comm)
+        score_matrix = dispatch_prediction(sequences, training_hsps, peptide_hsps, training_pairs, comm, args)
+
+        @info "Computing the fitness of the new peptides..."
+        compute_fitness!(offspring, args["target_id"], score_matrix, fitness_function)
+
+        duration = time() - start_time
+        @info "ITERATION $(generation) COMPLETED IN $(duration) seconds!"
+
+        @info "Logging the results..."
+        update_summary(summary_file, generation, duration, offspring, seed)
+        save_peptides_csv("$(run_dir)/peptides/gen$(generation).csv", offspring)
+        save_peptides_fasta("$(run_dir)/peptides/gen$(generation).fasta", offspring)
+
+        if args["save_matrices"]
+            FileIO.save_matrix("$(run_dir)/matrices/gen$(generation).mat")
+        end
+
+        peptides = [copy(peptide) for peptide in offspring]
+        generation += 1
+
+        # Check for convergence
+        if isconverged(summary_file, args)
+            @info "======== CONVERGED after $(generation - 1) generations ========"
+            return
+        end
+    end
 end
 
-function slave_process(comm, args)
+function main_process(comm, args)
+
+    rank = MPI.Comm_rank(comm)
+
+    @info "Loading the proteome sequences..."
+    proteins = FileIO.load_sequences(args["fasta"])
+
+    @info "Loading the training pairs..."
+    training_pairs = FileIO.load_training_pairs(args["training_pairs"])
+
+    @info "Loading the HSPs extracted from the proteome..."
+    training_hsps = FileIO.load_hsps(args["hsps"], proteins)
+    @info "Loaded $(length(training_hsps)) HSPs..."
+
+    @info "Creating a directory structure for the evolution results..."
+    if isdir(args["output"])
+        @error "$(args["output"]) already exists and cannot be overwritten."
+        return
+    end
+
+    output_dir = args["output"]
+    mkdir(output_dir)
+
+    for i in 1:args["n_runs"]
+        run(i, comm, args)
+    end
+end
+
+function worker_process(comm, args)
     rank = MPI.Comm_rank(comm)
     world_size = MPI.Comm_size(comm)
 
@@ -122,12 +225,13 @@ function slave_process(comm, args)
 
     @info "Loading the HSPs extracted from the proteome..."
     training_hsps = FileIO.load_hsps(args["hsps"], proteins)
-    @info "Loaded $(length(hsps)) HSPs..."
+    @info "Loaded $(length(training_hsps)) HSPs..."
 
     while true
         payload, status = MPI.recv(0, MPI.MPI_ANY_TAG, comm)
 
         if status.tag == EXTRACTION_TAG
+            @info "Worker $(MPI.Comm_rank(comm)) received sequences for HSP extraction..."
             hsps = Extraction.extract_hsps(
                 payload;
                 smer_sim_threshold = args["smer_similarity_threshold"],
@@ -140,11 +244,14 @@ function slave_process(comm, args)
                 num_processes = MPI.Comm_size(comm),
                 as_matrix = false
             )
+            @info "Worker $(MPI.Comm_rank(comm)) sending HSPs back..."
             MPI.send(hsps, 0, 1, comm)
         else
+            @info "Worker $(MPI.Comm_rank(comm)) received HSPs for scoring..."
+            merged_hsps = union(payload.hsps, training_hsps)
             matrix = Prediction.score_interactions(
                 payload.sequences,
-                payload.hsps,
+                merged_hsps,
                 training_pairs[rank+1:MPI.Comm_size(comm):end],
                 args["kmer_size"]
             )
@@ -257,8 +364,8 @@ if abspath(PROGRAM_FILE) == @__FILE__
     comm = MPI.COMM_WORLD
 
     if MPI.Comm_rank(comm) == 0
-        master_process(comm, args)
+        main_process(comm, args)
     else
-        slave_process(comm, args)
+        worker_process(comm, args)
     end
 end
